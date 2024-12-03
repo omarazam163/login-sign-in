ab4cc7ecf2bfefb5f8d663ee7a9b5df4435d8d2b
omarazam163 <omarazam163@email.com>
2024-12-03 04:58:57 +0200

    fixed a bug

--- js/signin.js
+++ js/signin.js
@@ -2,20 +2,33 @@ let email = document.getElementById("email");
 let password = document.getElementById("password");
 let btn = document.getElementById("submit");
 let users = JSON.parse(localStorage.getItem("users")) || [];
-let error = document.getElementById("error");
+let error = document.getElementById("error-email");
+let errorPass = document.getElementById("error-pass");
+
 
 btn.addEventListener("click", (e) => {
     e.preventDefault();
-    if (users.find((e) => e.email === email.value.trim())) {
-        if (users.find((e) => e.password === password.value.trim())) {
-            localStorage.setItem("userName", JSON.stringify(users.find((e) => e.email === email.value).name));
-            window.open("./welcome.html", "_self");
+    if(users.find(function (e) { return e.email==email.value.trim()}))
+    {
+        let userObj= users.find(function (e) {
+          return e.email == email.value.trim();
+        });
+        if(userObj.password == password.value.trim())
+        {
             error.classList.add("d-none");
-        } else {
-            error.classList.remove("d-none");
+            errorPass.classList.add("d-none");
+            localStorage.setItem("userName", JSON.stringify(userObj.name));
+            window.open('./welcome.html','_self');
         }
-    } else {
-        error.classList.remove("d-none");
+        else
+        {
+            error.classList.add("d-none");
+            errorPass.classList.remove("d-none");
+        }
     }
-    password.value="";
+    else
+    {
+        error.classList.remove("d-none");
+        console.log('email not found');
+    }
 });
